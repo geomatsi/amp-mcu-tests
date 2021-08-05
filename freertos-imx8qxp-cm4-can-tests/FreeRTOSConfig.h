1e943b49f1c6ec8f8df33530e7bca97627ec20c9
Sergey Matyukevich <geomatsi@gmail.com>
2021-08-05 14:09:42 +0300

    rtos: print assert details for debug purposes
    Implement debug assert versions that print location of the failed assert
    to simplify debugging. Disable debug assert versions by default.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- freertos-imx8qxp-cm4-can-tests/FreeRTOSConfig.h
+++ freertos-imx8qxp-cm4-can-tests/FreeRTOSConfig.h
@@ -97,7 +97,15 @@
 #define configTIMER_QUEUE_LENGTH                10
 #define configTIMER_TASK_STACK_DEPTH            (configMINIMAL_STACK_SIZE * 2)
 
+#if 0
+#define configASSERT_BOOL(x) if(( x) == 0) { \
+	DbgConsole_Printf("assert: %s:%d\r\n", __func__, __LINE__); \
+	taskDISABLE_INTERRUPTS(); for (;;); \
+}
+#else
 #define configASSERT_BOOL(x) if(( x) == 0) {taskDISABLE_INTERRUPTS(); for (;;);}
+#endif
+
 #define configASSERT(x) configASSERT_BOOL((x)!=0)
 
 /* Optional functions - most linkers will remove unused functions anyway. */
