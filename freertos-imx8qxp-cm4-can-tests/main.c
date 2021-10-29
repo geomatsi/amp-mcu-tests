5ed1cd1dfbe0339c21933e1dd3afa34c000f3e5d
Sergey Matyukevich <geomatsi@gmail.com>
2021-10-30 01:02:04 +0300

    mgmt: add support for events
    Add another management mechanism in addition to commands from i.MX8 host.
    Implement events that are initiated on CM4 core. This commit adds all the
    infrastructure to support events. CM4 prepares event buffer and notifies
    i.MX8 using mailbox. Linux kernel driver on host handles events and acks
    event using mailbox. So far only heartbeat event is defined for testing
    purposes. In general events can be used to send CAN error notifications
    to host as well as stop Tx queues on host in the case of any
    transmission issues.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- freertos-imx8qxp-cm4-can-tests/main.c
+++ freertos-imx8qxp-cm4-can-tests/main.c
@@ -40,10 +40,14 @@ uint32_t remote_addr = 0xbeef;
 
 static char stats[512];
 
-static mgmt_data_t mgmt_handler = {
-	.name = "mgmt_task",	
+static mgmt_data_t cmd_handler = {
+	.name = "cmd_task",
 };
 
+static mgmt_data_t evt_handler = {
+	.name = "evt_task",
+};
+ 
 /* callbacks */
 
 static void app_nameservice_isr_cb(uint32_t new_ept, const char *new_ept_name, uint32_t flags, void *user_data)
@@ -70,9 +74,18 @@ int32_t LSIO_MU13_INT_B_IRQHandler(void)
 	switch (type)
 	{
 	case CAN_RPMSG_CTRL_CMD:
-		if (mgmt_handler.queue)
+		if (cmd_handler.cmd.cmdq)
 		{
-			if ( pdTRUE != xQueueSendFromISR( mgmt_handler.queue, &size, NULL ))
+			if ( pdTRUE != xQueueSendFromISR( cmd_handler.cmd.cmdq, &size, NULL ))
+			{
+				(void)PRINTF("%s: failed to handle signal 0x%x\r\n", __func__, signal);
+			}
+		}
+		break;
+	case CAN_RPMSG_CTRL_ACK:
+		if (evt_handler.evt.ackq)
+		{
+			if ( pdTRUE != xQueueSendFromISR( evt_handler.evt.ackq, &size, NULL ))
 			{
 				(void)PRINTF("%s: failed to handle signal 0x%x\r\n", __func__, signal);
 			}
@@ -88,7 +101,7 @@ int32_t LSIO_MU13_INT_B_IRQHandler(void)
 
 /* freertos tasks */
 
-static void mgmt_task(void *param)
+static void cmd_task(void *param)
 {
 	const struct can_rpmsg_cmd *cmd = (struct can_rpmsg_cmd *)CTRL_MEM_REQ;
 	struct can_rpmsg_rsp *rsp = (struct can_rpmsg_rsp *)CTRL_MEM_RSP;
@@ -100,11 +113,9 @@ static void mgmt_task(void *param)
 
 	(void)PRINTF("%s: start...\r\n", priv->name);
 
-	/* process mgmt tasks */
-
 	while (1)
 	{
-		if (xQueueReceive(priv->queue, &msg, portMAX_DELAY) == pdFALSE)
+		if (xQueueReceive(priv->cmd.cmdq, &msg, portMAX_DELAY) == pdFALSE)
 		{
 			(void)PRINTF("%s: failed to recieve command...\r\n", priv->name);
 			continue;
@@ -205,7 +216,6 @@ static void mgmt_task(void *param)
 						break;
 					}
 
-
 					if (handler->ops.ifup)
 					{
 						ret = handler->ops.ifup(handler);
@@ -215,7 +225,6 @@ static void mgmt_task(void *param)
 							break;
 						}
 					}
-
 
 					if (handler->stb.present)
 					{
@@ -261,7 +270,7 @@ static void mgmt_task(void *param)
 
 					if (xSemaphoreTake(handler->runsem, portMAX_DELAY) != pdTRUE)
 					{
-						(void)PRINTF("%s: failed to give semaphore\r\n", __func__);
+						(void)PRINTF("%s: failed to take semaphore\r\n", __func__);
 					}
 
 					if (handler->stb.present)
@@ -295,7 +304,7 @@ static void mgmt_task(void *param)
 					rsp->id = cmd->id;
 					rsp->result = -ENOSYS;
 
-					(void)PRINTF("%s: unkonwn cmd: %d\r\n", priv->name, cmd->id);
+					(void)PRINTF("%s: unknown cmd: %d\r\n", priv->name, cmd->id);
 				}
 
 				break;
@@ -306,6 +315,60 @@ static void mgmt_task(void *param)
 	}
 
 	(void)PRINTF("%s: done...\r\n", priv->name);
+
+	while (1)
+	{
+	}
+}
+
+static void evt_task(void *param)
+{
+	struct can_rpmsg_evt *evt = (struct can_rpmsg_evt *)CTRL_MEM_EVT;
+	struct mgmt_data *priv = (struct mgmt_data *)param;
+	uint32_t beat = 0;
+	unsigned int event_id;
+	unsigned int ack;
+	uint32_t size;
+
+	(void)PRINTF("%s: start...\r\n", priv->name);
+
+	while (1)
+	{
+		if (xQueueReceive(priv->evt.evtq, &event_id, portMAX_DELAY) == pdFALSE)
+		{
+			(void)PRINTF("%s: recv failed...\r\n", priv->name);
+			continue;
+		}
+
+		memset((void *)evt, 0x0, CTRL_MEM_SIZE);
+
+		switch (event_id)
+		{
+			case CAN_RPMSG_EVT_HB:
+				{
+					struct can_rpmsg_evt_hb *e = (struct can_rpmsg_evt_hb *)evt;
+					size = sizeof(struct can_rpmsg_evt_hb);
+
+					e->hdr.hdr.type = CAN_RPMSG_CTRL_EVT;
+					e->hdr.hdr.len = sizeof(struct can_rpmsg_evt_hb);
+					e->hdr.id = event_id;
+					e->beat = beat++;
+				}
+
+				__DSB();
+				break;
+			default:
+				(void)PRINTF("%s: unknown evt: %u\r\n", priv->name, event_id);
+				continue;
+		}
+
+		MU_SendMsg(LSIO__MU13_B, CTRL_MU_CHAN, can_rpmsg_to_sig(CAN_RPMSG_CTRL_EVT, size));
+
+		if (xQueueReceive(priv->evt.ackq, &ack, portTICK_PERIOD_MS * 1000) == pdFALSE)
+		{
+			(void)PRINTF("%s: event %u ack timeout...\r\n", priv->name, event_id);
+		}
+	}
 
 	while (1)
 	{
@@ -382,21 +445,48 @@ int main(void)
 		}
 	}
 
-	/* mgmt */
+	/* cmd handler */
 
-
-	mgmt_handler.queue = xQueueCreate(1, sizeof( unsigned int ) );
-	if( mgmt_handler.queue == NULL )
+	cmd_handler.cmd.cmdq = xQueueCreate(1, sizeof( unsigned int ) );
+	if( cmd_handler.cmd.cmdq == NULL )
 	{
-		(void)PRINTF("failed to create mgmt queue\r\n");
+		(void)PRINTF("failed to create cmd queue\r\n");
 		while (1)
 		{
 		}
 	}
 
-	if (xTaskCreate(mgmt_task, "mgmt_task", APP_TASK_STACK_SIZE, &mgmt_handler, tskIDLE_PRIORITY + 2U, &mgmt_handler.task) != pdPASS)
+	if (xTaskCreate(cmd_task, "cmd_task", APP_TASK_STACK_SIZE, &cmd_handler, tskIDLE_PRIORITY + 2U, &cmd_handler.task) != pdPASS)
 	{
-		(void)PRINTF("failed to create mgmt task\r\n");
+		(void)PRINTF("failed to create cmd task\r\n");
+		while (1)
+		{
+		}
+	}
+
+	/* evt handler */
+
+	evt_handler.evt.evtq = xQueueCreate(5, sizeof( unsigned int ) );
+	if( evt_handler.evt.evtq == NULL )
+	{
+		(void)PRINTF("failed to create evt queue\r\n");
+		while (1)
+		{
+		}
+	}
+
+	evt_handler.evt.ackq = xQueueCreate(1, sizeof( unsigned int ) );
+	if( evt_handler.evt.ackq == NULL )
+	{
+		(void)PRINTF("failed to create evt ack queue\r\n");
+		while (1)
+		{
+		}
+	}
+
+	if (xTaskCreate(evt_task, "evt_task", APP_TASK_STACK_SIZE, &evt_handler, tskIDLE_PRIORITY + 2U, &evt_handler.task) != pdPASS)
+	{
+		(void)PRINTF("failed to create evt task\r\n");
 		while (1)
 		{
 		}
